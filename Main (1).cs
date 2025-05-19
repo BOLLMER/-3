0c918b4079cbb80be5cb4467a4337a73d48a43a0
BOLLMER <162329816+BOLLMER@users.noreply.github.com>
2025-05-19 22:26:27 +0700

    номер2_c#

---
+++ Main (1).cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+class RomanToInt {
+    static int Convert(string s) {
+        var map = new Dictionary<char, int> {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        int total = 0, prev = 0;
+        for (int i = s.Length - 1; i >= 0; i--) {
+            int val = map[s[i]];
+            total += val < prev ? -val : val;
+            prev = val;
+        }
+        return total;
+    }
+
+    static void Main() {
+        Console.WriteLine(Convert("XIV")); // 14
+    }
+}
